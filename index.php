a7d0fbe52817ed110994465df380558ae6662280
Abdul Azeez <54161406+leoazeez@users.noreply.github.com>
2020-10-16 11:41:28 +1100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+<meta charset="utf-8">
+<meta name="description" content="SYSAD MIN OFFICIAL HOME PAGE">
+<meta name="keywords" content="sysad,sysadmin,system administrator">
+<meta name="author" content="azeez">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+
+
+<link rel="stylesheet" type="text/css" href="style/style.css">
+<title>SYSAD MIN</title>
+</head>
+
+<body id="index">
+  <?php 
+  $page="index";
+  include_once "inc/header.inc";
+  include_once "inc/nav.inc";
+?>
+
+<section>
+<div class="con1">
+<h2>
+Who are we?
+</h2>
+<h3>
+SYSAD MIN is a company created by people who wants to create a difference in the world.<br>
+We want people to enjoy their lives freely by letting a group of total strangers look after their privacy.<br>
+we understand what it means to move forward.
+</h3>
+</div>
+<div class="con2">
+<h2>So... Why work with us?</h2>
+<h3>We have all the talent available all around the world. We have more resourcees than nasa itself. There are partners of SISAD MIN all around the world.<br>
+Lets say that you want to get a job done in Africa. We have people there. Even in the united states government.<br>
+Our stelth is so high that the FBI was amazed that we slipped right under their noses.<br>
+There are multiple possibilities if you plan to work with us.</h3>
+</div>
+<div class="con3">
+<h2>Are we trust worthy?</h2>
+<h3>Kind of<br>
+yeah............</h3>
+</div>
+<div class="con1">
+<h2>Disclaimer.</h2>
+<h3>We do not take responsibility to whatever our employees do with your personal data. 
+</h3>
+</div>
+</section>
+
+</body>
+
+<?php 
+	include_once "inc/footer.inc";
+?>
+
+</html>
